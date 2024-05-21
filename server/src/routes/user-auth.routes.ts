2d261eab76f64c52dc16663b43a3dd0feccbdfe7
pyropecs <praveengopu0105@gmail.com>
2024-05-21 10:26:55 +0530

    user authentication final with jwt token authentication

--- server/src/routes/user-auth.routes.ts
+++ server/src/routes/user-auth.routes.ts
@@ -2,24 +2,47 @@ import { Router } from "express";
 import { createUser, getUser } from "../models/user-auth.model";
 import { encryptPassword, checkUser } from "../utils";
 import jwt from "jsonwebtoken";
-const SECRET = String(process.env.SECRET);
-const router = Router();
 
+const router = Router();
 router.post("/register", async (req, res) => {
 	try {
 		const { username, password, email, role } = await req.body;
-		const hash = await encryptPassword(password);
-
-		const query = await createUser({
-			username,
-			password: hash,
-			email,
-			role,
-		});
-		res.status(200).send({
-			message: "user created successfully",
-			results: query,
-		});
+		const SECRET = process.env.SECRET;
+		if (username && password && email && role) {
+			const hash = await encryptPassword(password);
+			try {
+				const query = await createUser({
+					username,
+					password: hash,
+					email,
+					role,
+				});
+				if (SECRET) {
+					const token = jwt.sign({ username }, SECRET, {
+						expiresIn: "48h",
+					});
+					res.status(200).send({
+						message: "user created successfully",
+						results: { query, token },
+					});
+				} else {
+					console.log("there is no secret key in the environment");
+					res.status(500).send({ message: "internal server error" });
+				}
+			} catch (err: any) {
+				res.status(409).send({ message: err.detail });
+			}
+		} else {
+			console.log(
+				"body has no value or missing the requirements ",
+				req.originalUrl,
+				req.body,
+			);
+			res.status(400).send({
+				message:
+					"body has no value or missing the required valuees please ensure that these values username,email,password,role",
+			});
+		}
 	} catch (err) {
 		console.log(err);
 		res.status(500).send({ message: "internal server error" });
@@ -29,35 +52,36 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
 	try {
 		const { username, password } = await req.body;
-
+		const SECRET = process.env.SECRET;
 		const query = await getUser({ username, password });
 		const hash = query[0].password;
 		const dbUser = query[0].username;
-		const checkUserisValid: boolean = await checkUser(
-			username,
-			password,
-			hash,
-			dbUser,
-		);
+		try {
+			const checkUserisValid: boolean = await checkUser(
+				username,
+				password,
+				hash,
+				dbUser,
+			);
 
-		if (checkUserisValid) {
-			const token = jwt.sign({ username }, SECRET, {
-				expiresIn: "48h",
-			});
+			if (checkUserisValid && SECRET) {
+				const token = jwt.sign({ username }, SECRET, {
+					expiresIn: "48h",
+				});
 
-			res.status(200).send({
-				message: "user logged in successfully",
-				token: token,
-			});
-		} else {
-			res.status(401).send({ message: "please check the password" });
+				res.status(200).send({
+					message: "user logged in successfully",
+					token: token,
+				});
+			} else {
+				res.status(401).send({ message: "please check the password" });
+			}
+		} catch (err) {
+			res.status(400).send({ message: err });
 		}
 	} catch (err) {
-		console.log(err);
 		res.status(500).send({ message: "internal server error" });
 	}
 });
-
-
 
 export default router;
